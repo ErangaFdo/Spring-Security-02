7cf0c7faf834fe0cbeecd67230e64c3209fb1a3c
ErangaFernando404 <erangafdo1994@gmail.com>
2025-08-14 07:23:53 +0530

    setup file

---
+++ src/test/java/lk/ijse/gdse/springsecurity02/SpringSecurity02ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.springsecurity02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
