922ba822ed6c1a894a211be6bdcebcea8dfeafad
ErangaFernando404 <erangafdo1994@gmail.com>
2025-08-14 21:58:39 +0530

    create entity

---
+++ src/main/java/lk/ijse/gdse/springsecurity02/entity/Role.java
@@ -0,0 +1,5 @@
+package lk.ijse.gdse.springsecurity02.entity;
+
+public enum Role {
+    ADMIN,USER;
+}
---
+++ src/main/java/lk/ijse/gdse/springsecurity02/entity/User.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse.springsecurity02.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
