f3a859b760d3a5d88580b222bcae90f1adc63041
ErangaFernando404 <erangafdo1994@gmail.com>
2025-08-17 06:50:13 +0530

    create JwtAuthFilter & correction  entity

--- src/main/java/lk/ijse/gdse/springsecurity02/entity/User.java
+++ src/main/java/lk/ijse/gdse/springsecurity02/entity/User.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String username;
     private String password;
     @Enumerated(EnumType.STRING)
